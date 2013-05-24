ec10a8d3cc16d392c7facc54503c896284909905
Sean Carpenter <sean@seancarpenter.net>
2013-05-23 20:14:37 -0400

    Added comments to generate.rb.

--- generate.rb
+++ generate.rb
@@ -5,7 +5,9 @@ require "bundler/setup"
 
 require "haml"
 
+# Calls to "render" can take a context object that will be accessible from the templates.
 class Context
+  # Any properties of this object are available in the Haml templates.
   attr_reader :example_boolean
 
   def initialize(example_boolean, scope, options)
@@ -14,6 +16,7 @@ class Context
     @options = options
   end
 
+  # This is an example function that can be called inside Haml templates.
   def copyright_year
     start_year = 2013
     end_year = Time.now.year
@@ -24,12 +27,19 @@ class Context
     end
   end
 
+  # This function is no different from the "copyright_year" function above. It just uses some
+  # conventions to render another template file when it's called.
   def render_partial(file_name)
+    # The "default" version of the partial.
     file_to_render = "./views/partials/#{file_name.to_s}.haml"
     if @scope
+      # Look for a partial prefixed with the current "scope" (which is just the name of the
+      # primary template being rendered).
       scope_file = "./views/partials/#{@scope.to_s}_#{file_name.to_s}.haml"
+      # Use it if it's there.
       file_to_render = scope_file if File.exists? scope_file
     end
+    # If we found a matching partial (either the scoped one or the default), render it now.
     if File.exists? file_to_render
       partial = Haml::Engine.new(File.read(file_to_render), @options)
       partial.render
@@ -43,6 +53,7 @@ class Generator
   def initialize(example_boolean, output_dir = ".")
     @example_boolean = example_boolean
     @output_dir = output_dir
+    # Change these to whatever makes sense for your needs.
     @haml_options = { attr_wrapper: '"', format: :html5 }
   end
 
@@ -50,7 +61,10 @@ class Generator
     layout = Haml::Engine.new(File.read("./views/layout.haml"), @haml_options)
     c = Context.new @example_boolean, input_file, @haml_options
 
+    # If the file being processed by Haml contains a yield statement, the block passed to
+    # "render" will be called when it's hit.
     output = layout.render c do
+      # Render the actual page contents in place of the call to "yield".
       body = Haml::Engine.new(File.read("./views/#{input_file}.haml"), @haml_options)
       body.render c
     end
